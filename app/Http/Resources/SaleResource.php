0ac4c450e194b35d6e8cf745fafd9fcb6e7d0b07
Iskandar Isabekov <iskandarisabrkov@gmail.com>
2025-10-19 20:13:14 +0500

    date added in sales

--- app/Http/Resources/SaleResource.php
+++ app/Http/Resources/SaleResource.php
@@ -20,6 +20,7 @@ class SaleResource extends JsonResource
             'sales_type_id' => $this->sales_type_id,
             'summa' => $this->summa,
             'products' => ProductResource::collection($this->products),
+            'created_at' => $this->created_at->format('Y-m-d'),
         ];
     }
 }
