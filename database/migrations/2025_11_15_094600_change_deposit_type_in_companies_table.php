29a7b95fa0118c94818495c6f3ae980d1ab149eb
Iskandar Isabekov <iskandarisabrkov@gmail.com>
2025-11-19 19:37:56 +0500

    Deposit database problem solved

--- database/migrations/2025_11_15_094600_change_deposit_type_in_companies_table.php
+++ database/migrations/2025_11_15_094600_change_deposit_type_in_companies_table.php
@@ -1,26 +1,26 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
+        // Postgres uchun string -> decimal konvertatsiyasi
+        DB::statement('ALTER TABLE companies ALTER COLUMN deposit TYPE numeric(12,2) USING deposit::numeric(12,2)');
+
         Schema::table('companies', function (Blueprint $table) {
-            $table->decimal('deposit' , 12 , 2)->default(0)->change();
+            $table->decimal('deposit', 12, 2)->default(0)->change();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
+        DB::statement('ALTER TABLE companies ALTER COLUMN deposit TYPE varchar USING deposit::varchar');
+
         Schema::table('companies', function (Blueprint $table) {
             $table->string('deposit')->nullable()->change();
         });
