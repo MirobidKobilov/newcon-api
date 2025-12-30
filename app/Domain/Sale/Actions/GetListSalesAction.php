f781d748a672a542d380e067f5d1de48d576f2ef
Iskandar Isabekov <iskandarisabrkov@gmail.com>
2025-12-30 19:59:32 +0500

    Sale order by desc by created_at

--- app/Domain/Sale/Actions/GetListSalesAction.php
+++ app/Domain/Sale/Actions/GetListSalesAction.php
@@ -11,18 +11,19 @@ class GetListSalesAction
     public function __invoke(Request $request)
     {
         $validated = $request->validate([
-            'index' => 'nullable|integer|min:1',
-            'size' => 'nullable|integer|min:1',
+            'index'     => 'nullable|integer|min:1',
+            'size'      => 'nullable|integer|min:1',
             'from_date' => 'nullable|date',
-            'to_date' => 'nullable|date',
-            'search' => 'nullable|string',
+            'to_date'   => 'nullable|date',
+            'search'    => 'nullable|string',
         ]);
 
-        $from = $validated['from_date'] ?? null;
-        $to = $validated['to_date'] ?? null;
+        $from   = $validated['from_date'] ?? null;
+        $to     = $validated['to_date'] ?? null;
         $search = strtolower($validated['search'] ?? '');
 
-        $query = Sale::with(['company', 'products' , 'user']);
+        $query = Sale::with(['company', 'products', 'user'])
+            ->orderBy('created_at', 'desc');
 
         if ($from && $to) {
             $query->whereBetween('created_at', [$from, $to]);
@@ -41,9 +42,10 @@ class GetListSalesAction
         if (isset($validated['index']) || isset($validated['size'])) {
             $page = $validated['index'] ?? 1;
             $size = $validated['size'] ?? 10;
+
             $sales = $query->paginate($size, ['*'], 'page', $page);
         } else {
-            $sales = $query->orderBy('created_at', 'desc')->get();
+            $sales = $query->get();
         }
 
         return SaleResource::collection($sales);
